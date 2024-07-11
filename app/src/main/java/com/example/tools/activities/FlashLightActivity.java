3a55d93db5250e31b3507220e79a7a87fc8effc1
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-07-11 16:08:46 +0500

    added flashlight activity

---
+++ app/src/main/java/com/example/tools/activities/FlashLightActivity.java
@@ -0,0 +1,16 @@
+package com.example.tools.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tools.R;
+
+public class FlashLightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_light);
+    }
+}
