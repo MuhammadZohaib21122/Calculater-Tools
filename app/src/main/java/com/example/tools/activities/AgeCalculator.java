efd149b0a51012b03bc4129028e0406270059255
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-08-01 11:26:02 +0500

    remove old agecalculator method
    in this i can remove age calculator method and adding the new age calculater activity and starting new mrthod because the previous method is very complix and lots of errors

--- app/src/main/java/com/example/tools/activities/AgeCalculator.java
+++ app/src/main/java/com/example/tools/activities/AgeCalculator.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.tools.R;
 
-public class AboutActivity extends AppCompatActivity {
+public class AgeCalculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_age_calculator);
     }
 }
